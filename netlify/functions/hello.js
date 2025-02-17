5617f26ce97bbf38c9345838b3eb36611a3e1980
Muatasim-Aswad <malasoad2021@gmail.com>
2025-02-17 15:44:30 +0100

    add query params

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,9 @@
 exports.handler = async function (event, context) {
+  //get request query parameters
+  const { start, end, number } = event.queryStringParameters;
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: "Hello World", start, end, number }),
   };
 };
