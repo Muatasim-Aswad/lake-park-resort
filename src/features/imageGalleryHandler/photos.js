05be25f605cd4406ef2a4ea5b1db03203bf9998b
Muatasim-Aswad <malasoad2021@gmail.com>
2025-02-24 16:05:46 +0100

    fix favicon and fonts loading

--- src/features/imageGalleryHandler/photos.js
+++ src/features/imageGalleryHandler/photos.js
@@ -1,4 +1,5 @@
 export default [
+  "https://res.cloudinary.com/dhhfikgdp/image/upload/v1740394766/images/578520307_x8r1kc.jpg",
   "https://res.cloudinary.com/dhhfikgdp/image/upload/v1715937573/images/gallery/photo1_pnavkp.jpg",
   "https://res.cloudinary.com/dhhfikgdp/image/upload/v1715937572/images/gallery/photo2_ui9snp.jpg",
   "https://res.cloudinary.com/dhhfikgdp/image/upload/v1715937573/images/gallery/photo3_bmi1av.jpg",
